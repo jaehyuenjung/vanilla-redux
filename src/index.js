2240d540cd01e1a793a23d58c05817e3af8f8c5f
jaehyuenjung <jaehyuen0613@gmail.com>
2021-08-23 11:11:14 +0900

    Actions

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ const plus = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-    return count;
+const countModifier = (count = 0, {type}) => {
+    if (type === "ADD") {
+        return count + 1;
+    } else if (type === "MINUS") {
+        return count - 1;
+    } else {
+        return count;
+    }
 };
 
 const countStore = createStore(countModifier);
