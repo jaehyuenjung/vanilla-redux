dd2dc3ee7cbba67822bccf6888a273c2e8ac7866
jaehyuenjung <jaehyuen0613@gmail.com>
2021-08-23 11:31:28 +0900

    Subscriptions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { createStore } from "redux";
 
-const plus = document.getElementById("add");
+const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
+
+number.innerText = 0;
 
 const countModifier = (count = 0, {type}) => {
     if (type === "ADD") {
@@ -15,3 +17,20 @@ const countModifier = (count = 0, {type}) => {
 };
 
 const countStore = createStore(countModifier);
+
+const onChange = () => {
+    number.innerText = countStore.getState();
+}
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+    countStore.dispatch({ type: "ADD" });
+}
+
+const handleMinus = () => {
+    countStore.dispatch({ type: "MINUS" });
+}
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
