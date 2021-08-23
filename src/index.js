30de1d47e810f15c0e5a0e80a9548a2df22b90c9
jaehyuenjung <jaehyuen0613@gmail.com>
2021-08-23 12:14:19 +0900

    Delete To Do part Two

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const reducer = (state = [], action) =>{
         case ADD_TODO:
             return [...state, {text: action.text, id: Date.now()}];
         case DELETE_TODO:
-            return [];
+            return state.filter(toDo => toDo.id !== action.id);
         default:
             return state;
     }
@@ -29,7 +29,7 @@ const reducer = (state = [], action) =>{
 const store = createStore(reducer);
 
 const dispatchDeleteToDo = (e) => {
-    const id = e.target.parentNode.id;
+    const id = parseInt(e.target.parentNode.id);
     store.dispatch(deleteToDo(id));
 }
 
