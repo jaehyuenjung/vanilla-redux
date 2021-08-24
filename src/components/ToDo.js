99eba8f2a6cd44a488a79fdd0a7aefab5d002cfd
jaehyuenjung <jaehyuen0613@gmail.com>
2021-08-24 12:45:24 +0900

    createSlice

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { actionCreators } from "../store";
+import { remove } from "../store";
 
 function ToDo({ text, id, onButtonClick }) {
     return (
@@ -14,7 +14,7 @@ function ToDo({ text, id, onButtonClick }) {
 
 function mapDispatchToProps(dispatch, ownProps) {
     return {
-        onButtonClick: () => dispatch(actionCreators.deleteToDo(ownProps.id)),
+        onButtonClick: () => dispatch(remove(ownProps.id)),
     };
 }
 
--- src/store.js
+++ src/store.js
@@ -1,22 +1,39 @@
 import { createStore } from "redux";
-import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
+import {
+    configureStore,
+    createAction,
+    createReducer,
+    createSlice,
+} from "@reduxjs/toolkit";
 
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
+// const addToDo = createAction("ADD");
+// const deleteToDo = createAction("DELETE");
 
 function getLocalStorage() {
     return JSON.parse(localStorage.getItem("toDos"));
 }
 
-const reducer = createReducer(getLocalStorage(), {
-    [addToDo]: (state, action) => {
-        state.push({ text: action.payload, id: Date.now() });
+// const reducer = createReducer(getLocalStorage(), {
+//     [addToDo]: (state, action) => {
+//         state.push({ text: action.payload, id: Date.now() });
+//     },
+//     [deleteToDo]: (state, action) =>
+//         state.filter((toDo) => toDo.id !== action.payload),
+// });
+
+const toDos = createSlice({
+    name: "toDosReducer",
+    initialState: getLocalStorage(),
+    reducers: {
+        add: (state, action) => {
+            state.push({ text: action.payload, id: Date.now() });
+        },
+        remove: (state, action) =>
+            state.filter((toDo) => toDo.id !== action.payload),
     },
-    [deleteToDo]: (state, action) =>
-        state.filter((toDo) => toDo.id !== action.payload),
 });
 
-const store = configureStore({ reducer });
+const store = configureStore({ reducer: toDos.reducer });
 
 function saveLocalStorage() {
     localStorage.setItem("toDos", JSON.stringify(store.getState()));
@@ -24,9 +41,6 @@ function saveLocalStorage() {
 
 store.subscribe(saveLocalStorage);
 
-export const actionCreators = {
-    addToDo,
-    deleteToDo,
-};
+export const { add, remove } = toDos.actions;
 
 export default store;
