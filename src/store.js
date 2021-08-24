52225ff655a6c5de44b83bbb782757b495b7b448
jaehyuenjung <jaehyuen0613@gmail.com>
2021-08-24 12:18:41 +0900

    createReducer

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
@@ -8,16 +8,24 @@ function getLocalStorage() {
     return JSON.parse(localStorage.getItem("toDos"));
 }
 
-const reducer = (state = getLocalStorage(), action) => {
-    switch (action.type) {
-        case addToDo.type:
-            return [{ text: action.payload, id: Date.now() }, ...state];
-        case deleteToDo.type:
-            return state.filter((toDo) => toDo.id !== action.payload);
-        default:
-            return state;
-    }
-};
+// const reducer = (state = getLocalStorage(), action) => {
+//     switch (action.type) {
+//         case addToDo.type:
+//             return [{ text: action.payload, id: Date.now() }, ...state];
+//         case deleteToDo.type:
+//             return state.filter((toDo) => toDo.id !== action.payload);
+//         default:
+//             return state;
+//     }
+// };
+
+const reducer = createReducer(getLocalStorage(), {
+    [addToDo]: (state, action) => {
+        state.push({ text: action.payload, id: Date.now() });
+    },
+    [deleteToDo]: (state, action) =>
+        state.filter((toDo) => toDo.id !== action.payload),
+});
 
 const store = createStore(reducer);
 
